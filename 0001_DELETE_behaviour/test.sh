59a1796269e40221d9cbe6307b77081e1d54a922
Daniel Voina <dvoina@outlook.com>
2020-01-24 14:05:44 +0200

    Added also Apache

--- 0001_DELETE_behaviour/test.sh
+++ 0001_DELETE_behaviour/test.sh
@@ -1,3 +1,11 @@
 docker-compose -f "docker-compose.yml" up -d --build
-curl http://localhost/delete.me
-curl -X DELETE http://localhost/delete.me
+echo "Testing NginX"
+curl http://localhost:80/delete.me
+echo
+curl -X DELETE http://localhost:80/delete.me
+echo "Testing Apache"
+curl http://localhost:81/delete.me
+echo
+curl -X DELETE http://localhost:81/delete.me
+docker-compose down
+./test.sh
